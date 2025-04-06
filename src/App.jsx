9f5762afd6950639b949c56964db084ce9445376
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-04-06 17:28:31 -0400

    Greetings

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Greetings from "./components/Greetings";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Our Store</h1>
+      <Greetings name="Jaquarius Fondingle" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Greetings.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Greetings({name}) {
+    return (
+        <div>
+        <h3>Hello, {name}!</h3>
+        <p>Please take a look at our products</p>
+        </div>
+    );
+}
+
+export default Greetings;
